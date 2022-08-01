546a04fa146c16fc532fb47569d993e9e82c856d
ferazoguerrero <ferazoguerrero@twilio.com>
2022-08-01 11:41:59 -0500

    showing access token or error in deeplink result screen

--- login_experiment/src/main/java/com/twilio/login_experiment/DeeplinkResultActivity.kt
+++ login_experiment/src/main/java/com/twilio/login_experiment/DeeplinkResultActivity.kt
@@ -2,7 +2,10 @@ package com.twilio.login_experiment
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.ProgressBar
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -15,12 +18,24 @@ import retrofit2.converter.gson.GsonConverterFactory
 class DeeplinkResultActivity : AppCompatActivity() {
 
   private lateinit var service: Service
-  private lateinit var resultText: TextView
   private var code = ""
+  private lateinit var title: TextView
+  private lateinit var myBalance: Button
+  private lateinit var settings: Button
+  private lateinit var transfer: Button
+  private lateinit var products: Button
+  private lateinit var progressBar: ProgressBar
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_deeplink_result)
+
+    title = findViewById(R.id.owlBankTitle)
+    myBalance = findViewById(R.id.myBalance)
+    settings = findViewById(R.id.settings)
+    transfer = findViewById(R.id.transfer)
+    products = findViewById(R.id.products)
+    progressBar = findViewById(R.id.progressBar)
 
     val retrofit = Retrofit.Builder()
       .baseUrl(getString(R.string.request_evurl_url))
@@ -28,21 +43,23 @@ class DeeplinkResultActivity : AppCompatActivity() {
       .build()
     service = retrofit.create(Service::class.java)
 
-    resultText = findViewById(R.id.resultText)
     code = intent.data?.getQueryParameter("code").toString()
     if (code.isNotEmpty()) {
-      resultText.text = "code: ${intent.data?.getQueryParameter("code")}"
       invokeGetAccessToken()
     }
   }
 
-  private fun showSuccess() {
-    findViewById<TextView>(R.id.title).visibility = View.VISIBLE
+  private fun stopLoading() {
+    title.visibility = View.VISIBLE
+    myBalance.visibility = View.VISIBLE
+    settings.visibility = View.VISIBLE
+    transfer.visibility = View.VISIBLE
+    products.visibility = View.VISIBLE
+    progressBar.visibility = View.GONE
   }
 
   private fun invokeGetAccessToken() {
     CoroutineScope(Job()).launch(Dispatchers.Main) {
-
       try {
         val accessTokenResponse = withContext(Dispatchers.IO) {
           service.requestAccessToken(
@@ -51,12 +68,19 @@ class DeeplinkResultActivity : AppCompatActivity() {
             code = code
           )
         }
-        resultText.text =
-          resultText.text.toString() + "\nAccess token: ${accessTokenResponse.accessToken}"
-        showSuccess()
+        Toast.makeText(
+          this@DeeplinkResultActivity,
+          "Success! Access token: ${accessTokenResponse.accessToken}",
+          Toast.LENGTH_LONG,
+        ).show()
       } catch (exception: Exception) {
-        resultText.text =
-          resultText.text.toString() + "\nError getting access token: ${exception.message}"
+        Toast.makeText(
+          this@DeeplinkResultActivity,
+          "Error getting access token: ${exception.message}",
+          Toast.LENGTH_LONG,
+        ).show()
+      } finally {
+        stopLoading()
       }
     }
   }
