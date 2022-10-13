7c0132fce05c53ba61df90d41f0f1bbff7a9e378
Felipe Erazo <ferazoguerrero@twilio.com>
2022-10-13 10:18:40 -0500

    Add fonts and drawables, create layouts according to demo app in IOS.

--- sample/src/main/java/com/twilio/sample/MainActivity.kt
+++ sample/src/main/java/com/twilio/sample/MainActivity.kt
@@ -18,6 +18,7 @@ package com.twilio.sample
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import com.twilio.sample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -28,5 +29,6 @@ class MainActivity : AppCompatActivity() {
     super.onCreate(savedInstanceState)
     binding = ActivityMainBinding.inflate(layoutInflater)
     setContentView(binding.root)
+    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
   }
 }
